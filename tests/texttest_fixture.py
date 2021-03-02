5b6a9a964cf353f657469171c672e5ae7340f482
Izabela <izabelakowal@protonmail.com>
2021-03-02 23:47:12 +0100

    Updates

--- tests/texttest_fixture.py
+++ tests/texttest_fixture.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 
 from gilded_rose import *
 
-if __name__ == "__main__":
+
+def test_report():
     print ("OMGHAI!")
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
@@ -28,3 +29,5 @@ if __name__ == "__main__":
             print(item)
         print("")
         GildedRose(items).update_quality()
+
+    assert ["-------- day 0 --------", "name, sellIn, quality", "+5 Dexterity Vest, 10, 20", "Aged Brie, 2, 0", "Elixir of the Mongoose, 5, 7", "Sulfuras, Hand of Ragnaros, 0, 80", "Sulfuras, Hand of Ragnaros, -1, 80", "Backstage passes to a TAFKAL80ETC concert, 15, 20", "Backstage passes to a TAFKAL80ETC concert, 10, 49", "Backstage passes to a TAFKAL80ETC concert, 5, 49", "Conjured Mana Cake, 3, 6", "", "-------- day 1 --------", "name, sellIn, quality", "+5 Dexterity Vest, 9, 19", "Aged Brie, 1, 1", "Elixir of the Mongoose, 4, 6", "Sulfuras, Hand of Ragnaros, 0, 80", "Sulfuras, Hand of Ragnaros, -1, 80", "Backstage passes to a TAFKAL80ETC concert, 14, 21", "Backstage passes to a TAFKAL80ETC concert, 9, 50", "Backstage passes to a TAFKAL80ETC concert, 4, 50", "Conjured Mana Cake, 2, 5", ""] == report_lines
