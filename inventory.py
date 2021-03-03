5a5874cb52ef3cc70ba67d1befa0f6ffc201b2ab
izabelakowal <izabelakowal@protonmail.com>
2021-03-04 00:59:35 +0100

    More refactor

--- inventory.py
+++ inventory.py
@@ -1,36 +1,63 @@
 
+class Quality:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def degrade(self):
+        if self.amount > 0:
+            self.amount = self.amount - 1
+
+    def increase(self):
+        if self.amount < 50:
+            self.amount = self.amount + 1
+
+    def reset(self):
+        self.amount = 0
+
+    def is_less_than_50(self):
+        return self.amount < 50
+
+
 class Generic:
     def __init__(self, quality, sell_in):
-        self.quality = quality
+        self._quality = Quality(quality)
         self.sell_in = sell_in
 
+    @property
+    def quality(self):
+        return self._quality.amount
+
     def update(self):
-        if self.quality > 0:
-            self.quality = self.quality - 1
+        self._quality.degrade()
         self.sell_in = self.sell_in - 1
         if self.sell_in < 0:
-            if self.quality > 0:
-                self.quality = self.quality - 1
+            self._quality.degrade()
 
 
 class AgedBrie:
     def __init__(self, quality, sell_in):
-        self.quality = quality
+        self._quality = Quality(quality)
         self.sell_in = sell_in
 
+    @property
+    def quality(self):
+        return self._quality.amount
+
     def update(self):
-        if self.quality < 50:
-            self.quality = self.quality + 1
+        self._quality.increase()
         self.sell_in = self.sell_in - 1
         if self.sell_in < 0:
-            if self.quality < 50:
-                self.quality = self.quality + 1
+            self._quality.increase()
 
 
 class Sulfuras:
     def __init__(self, quality, sell_in):
-        self.quality = quality
+        self._quality = Quality(quality)
         self.sell_in = sell_in
+
+    @property
+    def quality(self):
+        return self._quality.amount
 
     def update(self):
         pass
@@ -38,18 +65,20 @@ class Sulfuras:
 
 class BackstagePass:
     def __init__(self, quality, sell_in):
-        self.quality = quality
+        self._quality = Quality(quality)
         self.sell_in = sell_in
 
+    @property
+    def quality(self):
+        return self._quality.amount
+
     def update(self):
-        if self.quality < 50:
-            self.quality = self.quality + 1
+        self._quality.increase()
+        if self._quality.is_less_than_50():
             if self.sell_in < 11:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
+                self._quality.increase()
             if self.sell_in < 6:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
+                self._quality.increase()
         self.sell_in = self.sell_in - 1
         if self.sell_in < 0:
-            self.quality = self.quality - self.quality
+            self._quality.reset()
