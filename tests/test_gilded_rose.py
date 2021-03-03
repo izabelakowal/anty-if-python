2b40ae5c14bb2732c9d3e0a90e77afa7bb09b7d1
izabelakowal <izabelakowal@protonmail.com>
2021-03-03 21:11:30 +0100

    Make tests green; add coverage lib

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -4,13 +4,65 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        assert items[0].name == "foo"
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "foo"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_report():
+    report_lines = []
+
+    items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
+
+    days = 2
+
+    for day in range(days):
+        report_lines.append("-------- day %s --------" % day)
+        report_lines.append("name, sellIn, quality")
+        for item in items:
+            report_lines.append(item)
+        report_lines.append("")
+        GildedRose(items).update_quality()
+
+    assert [
+               "'-------- day 0 --------'",
+               "'name, sellIn, quality'",
+               "+5 Dexterity Vest, 8, 18",
+               "Aged Brie, 0, 2",
+               "Elixir of the Mongoose, 3, 5",
+               "Sulfuras, Hand of Ragnaros, 0, 80",
+               "Sulfuras, Hand of Ragnaros, -1, 80",
+               "Backstage passes to a TAFKAL80ETC concert, 13, 22",
+               "Backstage passes to a TAFKAL80ETC concert, 8, 50",
+               "Backstage passes to a TAFKAL80ETC concert, 3, 50",
+               "Conjured Mana Cake, 1, 4",
+               "''",
+               "'-------- day 1 --------'",
+               "'name, sellIn, quality'",
+               "+5 Dexterity Vest, 8, 18",
+               "Aged Brie, 0, 2",
+               "Elixir of the Mongoose, 3, 5",
+               "Sulfuras, Hand of Ragnaros, 0, 80",
+               "Sulfuras, Hand of Ragnaros, -1, 80",
+               "Backstage passes to a TAFKAL80ETC concert, 13, 22",
+               "Backstage passes to a TAFKAL80ETC concert, 8, 50",
+               "Backstage passes to a TAFKAL80ETC concert, 3, 50",
+               "Conjured Mana Cake, 1, 4",
+               "''",
+           ] == [repr(item) for item in report_lines]
+
+
+def assert_backstage_pass_quality(expected, sell_in, quality):
+    pass
